964e44c4a4fb78fdd3345444e63341b7b4b91d5e
joao.cordeiro <joao.victor@nubbi.com.br>
2023-10-16 11:54:19 -0300

    altera estrutura do projeto

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require('dotenv').config()     
    
 const express = require('express')
-const router = require('./router/router')
+const router = require('./src/router/router')
 
 const app = express()
 
@@ -12,6 +12,7 @@ app.use(bodyParser.json())
 
 // Use EJS with view enginer
 app.set('view engine', 'ejs')
+app.set('views', __dirname + '/src/views');
 
 // Use public directory
 app.use(express.static('public'))
